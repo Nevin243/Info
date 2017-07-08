62c40eb43cc952e05fb53ce5176f98c9f62d08e8
Gareth Cairns <grpc@st-andrews.ac.uk>
2017-07-08 11:42:13 +0100

    Improved Model

--- Backend/ml_application.py
+++ Backend/ml_application.py
@@ -50,52 +50,20 @@ def train():
     training_data_reduced = sequence.pad_sequences(training_data_reduced, maxlen=review_len, value=0)
     test_data_reduced = sequence.pad_sequences(test_data_reduced, maxlen=review_len, value=0)
     # create model (training)
-    glove_path = "data/glove.6B.100d.txt"
-    embeddings_index = {}
-    f =  open(glove_path)
-
-    for line in f:
-        values = line.split() #by default split delimiter is a space
-        word = values[0] # first element in a line in Glove text file is the word 
-        weights = np.asarray(values[1:], dtype='float32') #get all weights for a word and save in an array
-        embeddings_index[word] = weights
-    f.close()
-
-    EMBEDDING_DIMENSION = 100
-
-    embedding_matrix = np.zeros((vocab_size, EMBEDDING_DIMENSION))
-    embedding_matrix = np.zeros((vocab_size, EMBEDDING_DIMENSION))
-    for i in range(1, len(embedding_matrix)):
-        word = indexToWord[i]
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:
-        # words not found in embedding index will be all-zeros.
-            embedding_matrix[i] = embedding_vector
-        else:
-            embedding_matrix[i] = np.random.normal(scale=0.6, size=(EMBEDDING_DIMENSION,))
-
-    conv3 = Sequential([
-        Embedding(vocab_size, 100, input_length=review_len, weights=[embedding_matrix], trainable=False),
-        SpatialDropout1D(0.2),
-        Dropout(0.25),
+    conv1 = Sequential([
+        Embedding(vocab_size, 32, input_length=review_len),
         Conv1D(64, 5, padding='same', activation='relu'),
-        Dropout(0.25),
         MaxPooling1D(),
         Flatten(),
         Dense(100, activation='relu'),
         Dropout(0.7),
         Dense(1, activation='sigmoid')])
-
-    conv3.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-    conv3.fit(training_data_reduced, training_labels, validation_data=(test_data_reduced, test_labels),
-          epochs=2, batch_size=64)
-    conv3.layers[0].trainable=True
-    conv3.fit(training_data_reduced, training_labels, validation_data=(test_data_reduced, test_labels),
-          epochs=1, batch_size=64)
+    conv1.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+    conv1.fit(training_data_reduced,training_labels, validation_data=(test_data_reduced, test_labels), nb_epoch=4, batch_size=64)
     # calculate accuracy etc - good for slides to show performance!
 
     # save model - to classify future data from a client
     # from sklearn.externals import joblib
     #joblib.dump(clf, 'Model/filename.pkl') 
 
-    return conv3
+    return conv1
